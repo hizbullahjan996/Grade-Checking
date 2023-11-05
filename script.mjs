14d0183f39939a6e786ad98847b6dd1b839fa622
hizbullahjan <hizbullahjan996@gmail.com>
2023-11-04 21:44:05 -0700

    Grade Checking

---
+++ script.mjs
@@ -0,0 +1,19 @@
+document.getElementById("checkButton").addEventListener("click", function() {
+    var percentage = parseFloat(document.getElementById("percentage").value);
+    var resultElement = document.getElementById("result");
+    var grade;
+
+    if (percentage >= 80) {
+        grade = "A+";
+    } else if (percentage >= 70) {
+        grade = "A";
+    } else if (percentage >= 60) {
+        grade = "B";
+    } else if (percentage >= 50) {
+        grade = "C";
+    } else {
+        grade = "F";
+    }
+
+    resultElement.textContent = "Grade: " + grade;
+});
